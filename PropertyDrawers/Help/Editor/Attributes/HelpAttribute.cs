c90e04655979505678d2ba7bb70e0366895231e7
devpilgrin <devpilgrim@gmail.com>
2016-04-12 22:12:29 +0800

    Added files via upload

---
+++ PropertyDrawers/Help/Editor/Attributes/HelpAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Editor.Attributes
+{
+    public class HelpAttribute : PropertyAttribute
+    {
+        public string HelpMessageText;
+        public HelpAttribute(string text)
+        {
+            HelpMessageText = text;
+        }
+    }
+}
---
+++ PropertyDrawers/Help/Editor/Drawers/HelpDrawer.cs
@@ -0,0 +1,43 @@
+﻿using Assets.Editor.Attributes;
+using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Editor.Drawers
+{
+    [CustomPropertyDrawer(typeof (HelpAttribute))]
+    public class HelpDrawer : PropertyDrawer
+    {
+
+        private int helpHeight = 50, textHeight = 16, HeaderY = 2;
+
+        private HelpAttribute helpAttribute
+        {
+            get { return (HelpAttribute) attribute; }
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (!string.IsNullOrEmpty(helpAttribute.HelpMessageText))
+            {
+                position.height = textHeight;
+
+                EditorGUI.PropertyField(position, property, label);
+
+                position.y += textHeight + HeaderY;
+                position.height = helpHeight;
+
+                EditorGUI.HelpBox(position, helpAttribute.HelpMessageText, MessageType.Info);
+            }
+            else
+            {
+                EditorGUI.PropertyField(position, property, label);
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return base.GetPropertyHeight(property, label) + helpHeight +
+                   (string.IsNullOrEmpty(helpAttribute.HelpMessageText) ? 0 : textHeight);
+        }
+    }
+}
