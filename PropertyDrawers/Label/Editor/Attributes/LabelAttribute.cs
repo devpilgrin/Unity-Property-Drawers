64b06fad084488ed54c83012e7bfbb8258227124
devpilgrin <devpilgrim@gmail.com>
2016-04-12 19:07:43 +0800

    Added files via upload

---
+++ PropertyDrawers/Label/Editor/Attributes/LabelAttribute.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Editor.Attributes
+{
+    /// <summary>
+    /// Label attribute - Атрибут позволяет заменить стандартный Label на собственную строку.
+    /// </summary>
+    public class LabelAttribute : PropertyAttribute
+    {
+        public string Text;
+        public LabelAttribute(string text)
+        {
+            Text = text;
+        }
+    }
+}
+ 
---
+++ PropertyDrawers/readme.md
@@ -0,0 +1,3 @@
+Коллекция Property Drawers:
+Label - Замена стандартного label на свой собственный.
+![](label_demo.png)
